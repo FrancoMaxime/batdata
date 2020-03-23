5a2fabb5bd07772ea16eede94844fb0a40086b32
FrancoMaxime <franco.maxime@gmail.com>
2020-03-23 12:31:23 +0100

    ajout new fichier

--- batmove.py
+++ batmove.py
@@ -1,7 +1,12 @@
 import csv
 import shutil
 
-with open('./csv/result_Tournay-Solvay_DataSM2.csv' + x) as csv_file:
+with open('./result/result_Tournay-Solvay_DataSM2.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
+    count = 0
     for row in csv_reader:
-        shutil.move("/storage/hd2/2015/SM2-B/Sons" + row["File"], "/storage/Tournay-Solvay-DataSM2" + row["File"])
+        if count == 0:
+            shutil.move("/storage/hd2/2015/SM2-B/Sons/" + row["File"], "/storage/Tournay-Solvay-DataSM2" + row["File"])
+            count = 42
+        else:
+            print("FLIP")
