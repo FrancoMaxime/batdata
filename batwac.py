fe00c619959ee74425ccb47d4eaf188d86468344
Maxime Franco <franco.maxime@gmail.com>
2020-04-20 18:16:57 +0200

    ajout find_wac

--- batwac.py
+++ batwac.py
@@ -23,7 +23,21 @@ def batwac(name):
                 val = tmp
     return found
 
-
+def find_wac(csv):
+    with open(csvname) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',')
+        for row in csv_reader:
+            name = None
+            if 'File' in row:
+                name = row['File']
+            elif 'Fichier' in row:
+                name = row['Fichier']
+            else:
+                print("ERROR 1 : No column File found")
+                
+            print(batwac(name))
+            
+        
 
 #print(batwac("B-BARBA_20160507_022854_000.wav"))
 print(batwac("B-BARBA_20160829_032146_626.wav"))
